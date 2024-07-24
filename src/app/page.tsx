ce293dc6b27aeea6f3900cd9eceb4d11387bcd9c
ElMubark A.Elwahab Hassan <Mobark.k11@outlook.com>
2024-07-24 22:13:48 +0300

    chore: Update package.json with @vercel/postgres dependency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+     {posts.map((post) => (
+      <>
+      <div key={post.id}>
+        <Link href={`/post/${post.id}`}>
+          {post.name}
+        </Link>
+      </div>
+        </>
+     ))}
       Hello 
     </main>
   );
